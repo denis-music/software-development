bd849b37ff746c6d867f4cccfd2fbaba1aca70ea
Denis Mušić <37282231+denis-music@users.noreply.github.com>
2019-10-28 08:31:08 +0100

    Add files via upload

---
+++ DLWMS.Web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DLWMS.Web.Models;
+using DLWMS.Service;
+using DLWMS.Core;
+
+namespace DLWMS.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        IStudentService studentService;
+
+        public HomeController(IStudentService studentService)
+        {
+            this.studentService = studentService;
+        }
+
+        public IActionResult Index()
+        {
+            Student s = studentService.GetStudentByIme("IB160062");
+            s?.Ime.ToUpper();
+            studentService.Update(s);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+
+            Student s = new Student() {  Ime = "Denis", Prezime = "Music"};
+            studentService.Add(s);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DLWMS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DLWMS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
