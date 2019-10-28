a9086aac65bec05eaad3df6e887ef69980cde4cf
Denis Mušić <37282231+denis-music@users.noreply.github.com>
2019-10-28 08:25:33 +0100

    Add files via upload

---
+++ DLWMS.Core/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DLWMS.Core
+{
+
+    
+    public class Student 
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+    }
+}
