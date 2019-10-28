c9e43f80139cb54a53be58047f59da0310cdef65
Denis Mušić <37282231+denis-music@users.noreply.github.com>
2019-10-28 08:26:46 +0100

    Add files via upload

---
+++ DLWMS.Repository/DLWMSDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DLWMS.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DLWMS.Repository
+{
+    public class DLWMSDbContext : DbContext
+    {
+
+        public DLWMSDbContext(DbContextOptions<DLWMSDbContext> opcije):base(opcije)
+        {
+
+        }
+        public virtual DbSet<Student> Students { get; set; }
+
+    }
+}
---
+++ DLWMS.Repository/Repository_DB.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DLWMS.Repository
+{
+
+    public interface IRepository<TEntity>
+    {
+        void Add(TEntity obj);
+        void Remove(TEntity obj);
+        void Update(TEntity obj);
+        IEnumerable<TEntity> GetAll();
+
+    }
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        DLWMSDbContext dbContext;
+        DbSet<TEntity> dbSet;
+
+        public Repository(DLWMSDbContext db)
+        {
+            dbContext = db;
+            dbSet = dbContext.Set<TEntity>();
+        }
+
+        public void Add(TEntity obj)
+        {
+            dbSet.Add(obj);
+            dbContext.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return dbSet.AsEnumerable();
+        }
+
+        public void Remove(TEntity obj)
+        {
+            dbSet.Remove(obj);
+            dbContext.SaveChanges();
+        }
+
+        public void Update(TEntity obj)
+        {
+            dbContext.Entry(obj).State = EntityState.Modified;
+            dbContext.SaveChanges();
+
+        }
+    }
+}
